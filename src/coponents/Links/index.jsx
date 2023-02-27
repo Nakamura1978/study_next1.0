994a62fdcf2f11ee108a1f5d5b31affa029c0534
Nakamura1978 <125366330+Nakamura1978@users.noreply.github.com>
2023-02-27 21:39:05 +0900

    ♻️ Links.module.cssのリファクタリング

--- src/coponents/Links/index.jsx
+++ src/coponents/Links/index.jsx
@@ -1,5 +1,5 @@
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import styles from '@/coponents/Links/Links.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 const ITEMS = [
