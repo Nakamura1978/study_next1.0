d4834f0ddaf8a0d7f621631f6079d1d04f5cce32
Nakamura1978 <125366330+Nakamura1978@users.noreply.github.com>
2023-02-28 21:21:25 +0900

    ✨ 共通headerの作成

---
+++ src/coponents/Header/index.jsx
@@ -0,0 +1,25 @@
+import Link from "next/link"
+import styles from "@/coponents/Header/Header.module.css"
+
+const NAV_ITEMS = [
+    {href : "/",label : "Index"},
+    {href : "/about",label : "About"},
+    {href : "/content",label : "Content"},
+    {href : "/product",label : "Product"},
+];
+
+export const Header = () => {
+    return(
+        <header className={styles.header}>
+            {NAV_ITEMS.map((item) => {
+                return (
+                    <Link
+                        key={item.href} href={item.href} className={styles.nav}
+                    >
+                        {item.label}
+                    </Link>
+                )
+            })}
+        </header>
+    )
+}
