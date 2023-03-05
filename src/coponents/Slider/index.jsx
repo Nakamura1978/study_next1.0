5495876680065a70d70c302101569a1ac77cb8a8
Nakamura1978 <125366330+Nakamura1978@users.noreply.github.com>
2023-03-05 17:23:44 +0900

    ✨ 記事コンポーネントの作成

---
+++ src/coponents/Slider/index.jsx
@@ -0,0 +1,9 @@
+import styles from "@/coponents/Slider/Slider.module.css"
+
+export const Slider = () => {
+    return(
+        <div className={styles.sliderBox}>
+            <img className={styles.imageBox} />
+        </div>
+    );
+}
